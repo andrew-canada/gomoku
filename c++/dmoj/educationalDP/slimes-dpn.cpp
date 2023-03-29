0bab94eb2905ab473b11c84a91e9e0e85731fc4e
Andrew Peng <andrew.peng.canada@gmail.com>
2023-03-28 22:24:42 -0400

    c++

---
+++ c++/dmoj/educationalDP/slimes-dpn.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <cstring>
+
+#define REP(i, a, b) for (int i = int(a); i <= int(b - 1); i++)
+#define min(a, b) a < b ? a : b
+
+typedef long long ll;
+
+int n;
+
+ll dp[405][405];
+ll psa[405];
+
+int main()
+{
+    scanf("%d", &n);
+    memset(dp, 0, sizeof(dp));
+    REP(i, 0, n)
+    {
+        scanf("%d", &psa[i]);
+        psa[i] += psa[i - 1];
+    }
+    REP(len, 2, n + 1)
+    {
+        REP(l, 0, n - len + 1)
+        {
+            ll r = l + len - 1;
+            REP(i, l, r + 1)
+            {
+                dp[l][r] = min(dp[l][r], );
+            }
+        }
+    }
+    printf("%d", dp[0][n - 1]);
+}
