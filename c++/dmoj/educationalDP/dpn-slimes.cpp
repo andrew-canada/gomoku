0d1057f8d04405c5bab39785c95514d3011330a8
Andrew Peng <andrew.peng.canada@gmail.com>
2023-03-29 22:01:29 -0400

    c++

--- c++/dmoj/educationalDP/dpn-slimes.cpp
+++ c++/dmoj/educationalDP/dpn-slimes.cpp
@@ -8,28 +8,37 @@ typedef long long ll;
 
 int n;
 
+int val;
 ll dp[405][405];
 ll psa[405];
 
 int main()
 {
     scanf("%d", &n);
-    memset(dp, 2147483647, sizeof(dp));
+    memset(dp, 1, sizeof(dp));
     REP(i, 0, n)
     {
-        scanf("%d", &psa[i]);
-        psa[i] += psa[i - 1];
+        scanf("%d", &val);
+        psa[i] = val + psa[i - 1];
+        dp[i][i] = 0;
     }
     REP(len, 2, n + 1)
     {
         REP(l, 0, n - len + 1)
         {
             ll r = l + len - 1;
-            REP(i, l, r + 1)
+            REP(i, l + 1, r + 1)
             {
-                dp[l][r] = min(dp[l][r], dp[l][i] + dp[i][r]);
+                dp[l][r] = min(dp[l][r], dp[l][i - 1] + dp[i][r]);
             }
-            dp[l][r] += psa[r] - psa[l];
+            if (r - l == 1 || r - l == n - 1)
+            {
+                dp[l][r] += psa[r] - psa[l - 1];
+            }
+            else
+            {
+                dp[l][r] += psa[r] - psa[l + 1];
+            }
         }
     }
     printf("%d", dp[0][n - 1]);
