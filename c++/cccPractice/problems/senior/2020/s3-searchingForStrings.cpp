4494f694325a0f6c1b29a0458c4804257ae455a2
Andrew Peng <andrew.peng.canada@gmail.com>
2023-05-27 23:14:40 -0400

    u

--- c++/cccPractice/problems/senior/2020/s3-searchingForStrings.cpp
+++ c++/cccPractice/problems/senior/2020/s3-searchingForStrings.cpp
@@ -11,6 +11,8 @@ int main()
     fgets(n, 200005, stdin);
     fgets(h, 200005, stdin);
     int len = strlen(n);
+    n[len - 1] = '\0';
+    std::sort(n, n + len - 1);
     do
     {
         if (strstr(h, n) != nullptr)
@@ -18,5 +20,5 @@ int main()
             num++;
         }
     } while (std::next_permutation(n, n + len - 1));
-    printf("%d", num + 1);
+    printf("%d", num);
 }
