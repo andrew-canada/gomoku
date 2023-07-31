54897f597667585c73bb0fa569ec1cf2f6252668
Andrew Peng <andrew.peng.canada@gmail.com>
2023-07-30 23:23:16 -0400

    c++

--- c++/cccPractice/problems/senior/2022/s4-goodTriplets.cpp
+++ c++/cccPractice/problems/senior/2022/s4-goodTriplets.cpp
@@ -6,14 +6,13 @@ typedef long long ll;
 
 int n, c;
 int pos;
-int pts[1000005];
+ll pts[1000005];
 int psa[1000005];
 ll nt;
 
 int main()
 {
     scanf("%d%d", &n, &c);
-    nt = n * (n - 1) * (n - 2) / 6;
     REP(i, 0, n)
     {
         scanf("%d", &pos);
@@ -24,7 +23,30 @@ int main()
     {
         psa[i] = psa[i - 1] + pts[i];
     }
-    REP(i, 0, c)
+    ll ln = n;
+    nt = ln * (ln - 1) * (ln - 2) / 6;
+    ll lc = c;
+    for (ll i = 0; i < lc; i++)
     {
+        ll opp = (i + lc / 2) % lc;
+        ll num = 0;
+        if (i < opp)
+        {
+            num = psa[opp] - (i + 1 == 0 ? 0 : psa[i]);
+        }
+        else
+        {
+            num = psa[opp] + psa[c - 1] - psa[i];
+        }
+        nt = nt - (pts[i] * num * (num - 1) / 2) - (num * pts[i] * (pts[i] - 1) / 2) - (pts[i] * (pts[i] - 1) * (pts[i] - 2) / 6);
     }
+    if (c % 2 == 0)
+    {
+        for (ll i = 0; i < lc / 2; i++)
+        {
+            ll opp = i + lc / 2;
+            nt = nt + (pts[opp] * pts[i] * (pts[i] - 1) / 2) + (pts[i] * pts[opp] * (pts[opp] - 1) / 2);
+        }
+    }
+    printf("%lld", nt);
 }
