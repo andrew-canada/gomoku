b5074bd99ba505c50ce7a0f4347ab6e369833acf
Andrew Peng <andrew.peng.canada@gmail.com>
2023-09-01 14:25:46 -0400

    c++

---
+++ c++/cccPractice/problems/senior/2022/s5-goodInfluencersReal.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define REP(i, a, b) for (int i = int(a); i <= int(b); i++)
+#define INF 200005
+
+int n;
+int s1, s2;
+char status[INF];
+bool yn[INF];
+int cost[INF];
+int dp[INF][3];
+vector<int> adjL[INF];
+
+void dfs(int cur, int par);
+
+int main()
+{
+    scanf("%d", &n);
+    REP(i, 1, n)
+    {
+        scanf("%d%d", &s1, &s2);
+        adjL[s1].push_back(s2);
+        adjL[s2].push_back(s1);
+    }
+    scanf("%s", &status);
+    REP(i, 1, n)
+    {
+        yn[i] = (status[i - 1] == 'Y');
+    }
+    REP(i, 1, n)
+    {
+        scanf("%d", &cost[i]);
+    }
+    dfs(1, -1);
+    printf("%d", min({dp[1][0], dp[1][1], dp[1][2]}));
+}
+
+void dfs(int cur, int par)
+{
+    dp[cur][0] = dp[cur][1] = dp[cur][2] = cost[cur];
+    for (int s : adjL[cur])
+    {
+        if (s == par)
+        {
+            continue;
+        }
+        dfs(s, cur);
+        dp[cur][0] += min({dp[s][0], dp[s][1], dp[s][2]});
+        dp[cur][2] += min({dp[s][0], dp[s][1], dp[s][2]});
+    }
+}
