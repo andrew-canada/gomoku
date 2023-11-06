090e610055a457cc47b7841856f50363e38a9ba3
Andrew Peng <andrew.peng.canada@gmail.com>
2023-11-05 22:36:41 -0500

    c++

--- c++/cco/2023/p1.cpp
+++ c++/cco/2023/p1.cpp
@@ -4,6 +4,10 @@
 
 int n, k;
 int sums[1000005];
+int dp[1000005][1000005];
+int num;
+
+int nCk(int n, int k);
 
 int main()
 {
@@ -12,4 +16,19 @@ int main()
     {
         scanf("%d", &sums[i]);
     }
+    num += nCk(k, sums[0]);
+    printf("%d", num);
+}
+
+int nCk(int n, int k)
+{
+    if (k == 0 || k == n)
+    {
+        return 1;
+    }
+    if (dp[n][k])
+    {
+        return dp[n][k];
+    }
+    return dp[n][k] = nCk(n - 1, k - 1) + nCk(n - 1, k);
 }
