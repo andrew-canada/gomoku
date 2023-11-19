e425406001bcf6e6c0d5f31887dd731ad6e25efd
Andrew Peng <andrew.peng.canada@gmail.com>
2023-11-19 13:52:50 -0500

    c++

---
+++ c++/cco/2023/p2/realMountains.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i, a, b) for (int i = int(a); i <= int(b); i++)
+
+int h[1000005];
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    REP(i, 1, n)
+    {
+        scanf("%d", &h[i]);
+    }
+}
