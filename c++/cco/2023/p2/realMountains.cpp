37f475f4f92805b5c7dd076e06446e242e65cdfa
Andrew Peng <andrew.peng.canada@gmail.com>
2023-11-27 10:04:22 -0500

    c++

--- c++/cco/2023/p2/realMountains.cpp
+++ c++/cco/2023/p2/realMountains.cpp
@@ -1,10 +1,18 @@
+// n columns of pixels of height Hn
+// peak is higher than left and right
+// result only has one peak
+// (i, j, k) -> peak j increments for cost Hi+Hj+Hk
+// minimize cost to create single peak
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 #define REP(i, a, b) for (int i = int(a); i <= int(b); i++)
 
-int h[1000005];
+const int MAXN = 1000005;
+int cost;
+int h[MAXN];
 
 int main()
 {
@@ -14,4 +22,32 @@ int main()
     {
         scanf("%d", &h[i]);
     }
+    int ch;
+    int peaks = 1;
+    while (peaks > 0)
+    {
+        peaks = 0;
+        REP(i, 1, n)
+        {
+            if (i - 1 >= 0 && i + 1 <= n)
+            {
+                if (h[i] > h[i - 1] && h[i] > h[i + 1])
+                {
+                }
+            }
+            if (i + 1 <= n)
+            {
+                if (h[i] > h[i + 1])
+                {
+                }
+            }
+            ch = max(h[i - 1], h[i + 1]);
+            if (ch > h[i])
+            {
+                peaks++;
+                h[i - 1] = h[i] = h[i + 1] = ch;
+            }
+        }
+    }
+    printf("%d", cost);
 }
