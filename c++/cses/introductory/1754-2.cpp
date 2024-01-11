54986c6b5de2a870cc3683b4602e09a78ba12ed9
Andrew Peng <andrew.peng.canada@gmail.com>
2024-01-11 15:40:33 -0500

    C++

---
+++ c++/cses/introductory/1754-2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a <= 2 * b && b <= 2 * a && (a + b) % 3 == 0)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+}
