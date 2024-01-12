701a08f10fc1a756429958d6d5d77cced19af567
Andrew Peng <andrew.peng.canada@gmail.com>
2024-01-12 10:16:12 -0500

    c++

---
+++ c++/cses/graph/1193-2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int mxN = 1e3;
+
+string s[mxN + 1];
+
+int main()
+{
+    int r, c;
+    cin >> r >> c;
+    for (int i = 0; i < r; i++)
+    {
+        cin >> s[i];
+    }
+}
