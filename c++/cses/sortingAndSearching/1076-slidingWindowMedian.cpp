4f79ae2917866820e0a25d9c894715b78d96d885
Andrew Peng <andrew.peng.canada@gmail.com>
2024-01-16 22:23:18 -0500

    c++

---
+++ c++/cses/sortingAndSearching/1076-slidingWindowMedian.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+const int mxN = 2e5;
+
+int arr[mxN + 1];
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+}
