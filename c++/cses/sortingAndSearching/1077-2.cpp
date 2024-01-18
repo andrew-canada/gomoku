1ed11b203ec1a51d26b499556ef9736e4e29c1ed
Andrew Peng <andrew.peng.canada@gmail.com>
2024-01-17 22:26:41 -0500

    c++

--- c++/cses/sortingAndSearching/1077-2.cpp
+++ c++/cses/sortingAndSearching/1077-2.cpp
@@ -10,21 +10,26 @@ int n, k;
 int arr[mxN + 1];
 array<int, 2> med = {-1};
 set<array<int, 2>> low, high;
+ll bs, ts;
 
 void fix()
 {
-    int nt = low.size() + high.size() + 1;
-    if (low.size() > (nt - 1) / 2)
+    ll te = low.size() + high.size() + 1;
+    if (low.size() > (te - 1) / 2)
     {
         high.insert(med);
+        ts += med[0];
         med = *--low.end();
         low.erase(med);
+        bs -= med[0];
     }
-    if (low.size() < (nt - 1) / 2)
+    if (low.size() < (te - 1) / 2)
     {
         low.insert(med);
+        bs += med[0];
         med = *high.begin();
         high.erase(med);
+        ts -= med[0];
     }
 }
 
@@ -38,10 +43,12 @@ void add(array<int, 2> cur)
     if (cur >= med)
     {
         high.insert(cur);
+        ts += cur[0];
     }
     else
     {
         low.insert(cur);
+        bs += cur[0];
     }
     fix();
 }
@@ -50,18 +57,19 @@ void rem(array<int, 2> cur)
 {
     if (cur == med)
     {
-        // med = *--low.end();
-        // low.erase(med);
         med = *high.begin();
         high.erase(med);
+        ts -= med[0];
     }
     else if (cur > med)
     {
         high.erase(cur);
+        ts -= cur[0];
     }
     else
     {
         low.erase(cur);
+        bs -= cur[0];
     }
     fix();
 }
@@ -77,7 +85,7 @@ int main()
     {
         for (int i = 0; i < n; i++)
         {
-            cout << arr[i] << " ";
+            cout << "0 ";
         }
         return 0;
     }
@@ -88,7 +96,7 @@ int main()
     for (int i = k - 1; i < n; i++)
     {
         add({arr[i], i});
-        cout << med[0] << " ";
+        cout << ts - (ll)high.size() * med[0] + (ll)low.size() * med[0] - bs << " ";
         rem({arr[i - k + 1], i - k + 1});
     }
 }
