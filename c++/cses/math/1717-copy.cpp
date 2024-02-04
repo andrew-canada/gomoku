cf8bafb85f2e93b2014537b31ee90059a60ffb04
Andrew Peng <andrew.peng.canada@gmail.com>
2024-02-03 22:19:25 -0500

    c++

--- c++/cses/math/1717-copy.cpp
+++ c++/cses/math/1717-copy.cpp
@@ -5,20 +5,20 @@ using namespace std;
 #define pb push_back
 #define INF 0x3f3f3f3f
 
-typedef long long ll;
+typedef unsigned long long ull;
 
 const int M = 1e9 + 7, mxN = 1e6;
 
-ll dp[mxN + 1];
+ull dp[mxN + 1];
 
 int main()
 {
-    ll n;
+    int n;
     cin >> n;
     dp[0] = 1;
     for (int i = 2; i <= n; i++)
     {
-        dp[i] = ((i - 1) * dp[i - 1] + (i - 1) * dp[i - 2]) % M;
+        dp[i] = (((i - 1) * dp[i - 1] % M) + ((i - 1) * dp[i - 2] % M)) % M;
     }
     cout << dp[n];
 }
