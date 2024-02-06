44e5ec390ff6a875dcb0827e745f871e1bf276d7
Andrew Peng <andrew.peng.canada@gmail.com>
2024-02-06 12:35:27 -0500

    c++

---
+++ c++/cses/math/1725-2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define INF 0x3f3f3f3f
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> mat;
+
+const int mxN = 100, mxX = 1e6, M = 1e9 + 7;
+
+int main()
+{
+    int n, a, b;
+    cin >> n >> a >> b;
+}
