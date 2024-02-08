8cca7ca07007c70828a3aae296f499c1c1281398
Andrew Peng <andrew.peng.canada@gmail.com>
2024-02-08 09:07:17 -0500

    c++

---
+++ c++/cses/sortingAndSearching/1077-2-2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define INF 0x3f3f3f3f
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> mat;
+
+const int mxN = 2e5, mxX = 1e6, M = 1e9 + 7, mxS = 600;
+
+int arr[mxN + 1];
+multiset<int> low, high;
+
+void fix()
+{
+}
+
+void add()
+{
+}
+
+void rem()
+{
+}
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+}
